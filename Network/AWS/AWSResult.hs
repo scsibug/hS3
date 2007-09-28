6bb0074c80026ddb032a72d20492451b35837182
Greg Heartsfield <scsibug@imap.cc>
2007-09-28 06:23:03 +0200

    Added haddock docs for AWSResult module.
    darcs-hash:20070928042303-1973b-97e83a5b380f3e962d4bc1a57a49a305cfcb121a

--- Network/AWS/AWSResult.hs
+++ Network/AWS/AWSResult.hs
@@ -18,13 +18,23 @@ module Network.AWS.AWSResult (
 
 import Network.Stream as Stream
 
-type AWSResult a = Either ReqError {- some kind of failure to process the request -}
-                          a {- result -}
+-- | A result from processing a request to S3.  Either some success
+--   value, or a 'ReqError'.
+type AWSResult a = Either ReqError a
+
+-- | An error from an S3 request, either at the network layer, or from
+--   S3 itself.
 data ReqError =
+    -- | Connection error at the network layer.
     NetworkError Stream.ConnError |
-    AWSError String String -- AWS error code and message
+    -- | @AWSError code message@ constructs an error message from S3
+    --   itself.  See
+    --   <http://docs.amazonwebservices.com/AmazonS3/2006-03-01/ErrorCodeList.html>
+    --   for a detailed list of possible codes.
+    AWSError String String
              deriving (Show, Eq)
 
+-- | Pretty print an error message.
 prettyReqError :: ReqError -> String
 prettyReqError r = case r of
                      AWSError a b -> b
